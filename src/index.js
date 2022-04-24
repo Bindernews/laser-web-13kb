bbfa45acef4bef24f0df114211faee1c063dad24
JulieBodette <jr3python@gmail.com>
2022-04-23 21:56:13 -0400

    Detect the X and Y position of the mouse and log to console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import {Game} from './game';
-document.addEventListener('load', () => {
+document.addEventListener('load', () => { //makes a new game window when the page loads
   window.game = new Game();
   window.game.run();
 }, true);
+
+
+document.addEventListener("mousemove", function(e) { 
+console.log("X "+e.clientX);  //print x mouse position
+console.log("Y "+e.clientY);  //print y mouse position
+
+});
